68d5b60fe7fa68c78c04634833513b01254e45fb
song96721 <78109443+song96721@users.noreply.github.com>
2021-01-29 00:34:55 -0800

    Create StarsAndStripes.java

---
+++ Unit 1/StarsAndStripes.java
@@ -0,0 +1,57 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 1/28/21
+//Class - APCSA Period 3
+//Lab  - StarsAndStripes
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+     out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+     out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+     out.println("");
+     out.println("");
+   }
+   
+   public void printASmallBox()
+   {	
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+   }
+ 
+   public void printABigBox()
+   { 	
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+     out.println("********************");
+     out.println("--------------------");
+     out.println("********************");
+   }   
+}
