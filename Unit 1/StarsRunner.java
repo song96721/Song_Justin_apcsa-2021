aa6bd3fcac11acff0a13ae5e1aa1724bf3f9dc54
song96721 <78109443+song96721@users.noreply.github.com>
2021-01-29 00:36:24 -0800

    Create StarsRunner.java

---
+++ Unit 1/StarsRunner.java
@@ -0,0 +1,25 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 1/28/21
+//Class - APCSA Period 3
+//Lab  - StarsRunner
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes s = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      s.printTwentyStars();
+      s.printTwentyDashes();
+      s.printTwoBlankLines();
+      s.printASmallBox();
+      s.printABigBox();
+      
+   }
+}
