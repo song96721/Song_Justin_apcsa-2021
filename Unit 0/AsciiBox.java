049ecc2ded62cd7eac4e4405838970c1232fe3d7
song96721 <78109443+song96721@users.noreply.github.com>
2021-01-28 22:43:42 -0800

    Create AsciiBox.java

---
+++ Unit 0/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 1/28/21
+//Class - APCSA Period 3
+//Lab  - AsciiBox
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Justin Song \t  1/28/21 \n\n" );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+    System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%% " );
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&& " );
+    System.out.println("------------------------- " );
+    System.out.println("+++++++++++++++++++++++++ " );
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> " );
+    System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<< " );
+    System.out.println("+++++++++++++++++++++++++ " );
+    System.out.println("+++++++++++++++++++++++++ " );
+    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+    System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%% " );
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&& " );
+    System.out.println("------------------------- " );
+    System.out.println("+++++++++++++++++++++++++ " );
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> " );
+    System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<< " );
+    System.out.println("+++++++++++++++++++++++++ " );
+    System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
