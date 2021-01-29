7ad5ccc1d1648a656f267c7e3a845fb20f823eeb
song96721 <78109443+song96721@users.noreply.github.com>
2021-01-29 00:06:04 -0800

    Create Variables.java

---
+++ Unit 0/Variables.java
@@ -0,0 +1,45 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 1/28/21
+//Class - APCSA Period 3
+//Lab  - Variables
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 117;
+    short shortOne = 43;
+    int intOne = 34;
+    long longOne = 68;
+
+		//decimal variables
+    float floatOne = 36.67948f;
+    double doubleOne = 46.983;
+
+		//other integer types
+    char charOne = 'B';
+
+		//other types
+		boolean booleanOne = false;
+		String stringOne = "Hello";
+
+		//output your information here
+		System.out.println("8-bit byteOne: " + byteOne);
+		System.out.println("16-bit shortOne "+ shortOne);
+		System.out.println("32-bit intOne: " + intOne);
+		System.out.println("64-bit longOne: " + longOne);
+		System.out.println("32-bit floatOne: " + floatOne);
+		System.out.println("64-bit doubleOne: " + doubleOne);
+		System.out.println("16-bit charOne: " + charOne);
+		System.out.println("booleanOne: " + booleanOne);
+		System.out.println("stringOne: " + stringOne);
