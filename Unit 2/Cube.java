dac32575c2d1e5d6ce02aa143f20d18c92350499
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-03 04:36:10 +0800

    Create Cube.java

---
+++ Unit 2/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Justin Song
+//Date - 2/2/21
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6* (side * side);
+	}
+}
