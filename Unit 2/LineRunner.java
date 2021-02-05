96b138e40c1098cadaa6f8cc68362b514b1d81a7
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-05 13:38:57 +0800

    Create LineRunner.java

---
+++ Unit 2/LineRunner.java
@@ -0,0 +1,19 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Justin Song
+//Date - 2/2/21
+//Class - APCSA Period 3
+//Lab  - LineRunner
+
+public class LineRunner
+{
+   public static void main( String[] args )
+   {
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,9,14,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,18,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 6,4,2,2 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 4,4,5,3 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,1,2,9 ) );
+		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,2,9 ) );
+		
+	}
+}
