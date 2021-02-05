97694eadab3d26f49c42cf76a84acee93dc44c7d
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-05 13:42:57 +0800

    Create Trap.java

---
+++ Unit 2/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Justin Song
+//Date - 2/2/21
+//Class - APCSA Period 3
+//Lab  - Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return 0.5*(base1 + base2)*height;
+
+	}
+}
