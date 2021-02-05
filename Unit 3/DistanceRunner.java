79397214eb4cb4ae6a8e907006306e5b466f9467
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-05 14:14:43 +0800

    Update DistanceRunner.java

--- Unit 3/DistanceRunner.java
+++ Unit 3/DistanceRunner.java
@@ -12,47 +12,47 @@ import static java.lang.Math.*;
 
 public class DistanceRunner
 {
-	public static void main( String[] args )
-	{
+  public static void main( String[] args )
+  {
     Scanner number = new Scanner(System.in);
 
     System.out.print("Enter X1 :: ");
-		int x1 = number.nextInt();
-		System.out.print("Enter Y1 :: ");
-		int y1 = number.nextInt();
-		System.out.print("Enter X2 :: ");
-		int x2 = number.nextInt();
-		System.out.print("Enter Y2 :: ");
-		int y2 = number.nextInt();
+    int x1 = number.nextInt();
+    System.out.print("Enter Y1 :: ");
+    int y1 = number.nextInt();
+    System.out.print("Enter X2 :: ");
+    int x2 = number.nextInt();
+    System.out.print("Enter Y2 :: ");
+    int y2 = number.nextInt();
 
     Distance dOne = new Distance(x1,y1,x2,y2);
     dOne.calcDistance();
     dOne.print();
 
     System.out.print("Enter X1 :: ");
-		x1 = number.nextInt();
-		System.out.print("Enter Y1 :: ");
-		y1 = number.nextInt();
-		System.out.print("Enter X2 :: ");
-		x2 = number.nextInt();
-		System.out.print("Enter Y2 :: ");
-		y2 = number.nextInt();
+     x1 = number.nextInt();
+    System.out.print("Enter Y1 :: ");
+     y1 = number.nextInt();
+    System.out.print("Enter X2 :: ");
+     x2 = number.nextInt();
+    System.out.print("Enter Y2 :: ");
+     y2 = number.nextInt();
 		
-		Distance dTwo = new Distance(x1,y1,x2,y2);
-		dTwo.calcDistance();
-		System.out.println(dTwo.toString());
+    Distance dTwo = new Distance(x1,y1,x2,y2);
+    dTwo.calcDistance();
+    System.out.println(dTwo.toString());
 	
-		System.out.print("Enter X1 :: ");
-		 x1 = number.nextInt();	
-		System.out.print("Enter Y1 :: ");
-		 y1 = number.nextInt();
-		System.out.print("Enter X2 :: ");
-		 x2 = number.nextInt();		
-		System.out.print("Enter Y2 :: ");
-		 y2 = number.nextInt();
+    System.out.print("Enter X1 :: ");
+    x1 = number.nextInt();	
+    System.out.print("Enter Y1 :: ");
+    y1 = number.nextInt();
+    System.out.print("Enter X2 :: ");
+    x2 = number.nextInt();		
+    System.out.print("Enter Y2 :: ");
+    y2 = number.nextInt();
 			
-		Distance dThree = new Distance(x1,y1,x2,y2);
-		dThree.calcDistance();
-		dThree.print();	
-	}
+    Distance dThree = new Distance(x1,y1,x2,y2);
+    dThree.calcDistance();
+    dThree.print();	
+  }
 }
