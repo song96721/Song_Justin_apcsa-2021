d3063b9894c42ac79920a3bf4d0fe7c8b26000ae
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-05 13:38:31 +0800

    Update Line.java

--- Unit 2/Line.java
+++ Unit 2/Line.java
@@ -5,9 +5,9 @@
 
 public class Line
 {
-	public static double getSlope( int x1, int y1, int x2, int y2 )
-	{
-		return (double)(y2-y1)/(double)(x2-x1);
-	}
+  public static double getSlope( int x1, int y1, int x2, int y2 )
+  {
+    return (double)(y2-y1)/(double)(x2-x1);
+  }
 
 }
