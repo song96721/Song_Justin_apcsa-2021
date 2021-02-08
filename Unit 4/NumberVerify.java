4c2586130ebf47f5505df75aa2c58448fd230b6a
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-07 20:32:59 -0800

    Create NumberVerify.java

---
+++ Unit 4/NumberVerify.java
@@ -0,0 +1,21 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 2/5/21
+//Class - APCSA Period 3
+//Lab  - NumberVerify
+
+import static java.lang.System.*;
+
+public class NumberVerify
+{
+	public static boolean isOdd( int num )
+	{
+		return !(num % 2 == 0);
+	}
+	public static boolean isEven( int num )
+	{
+		return (num % 2 == 0);
+	}	
+}
