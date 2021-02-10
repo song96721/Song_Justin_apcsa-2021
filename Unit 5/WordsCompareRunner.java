e3ded1043a9ee22acfc71beb259a643e4f9bd6c9
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-09 19:23:36 -0800

    Update WordsCompareRunner.java

--- Unit 5/WordsCompareRunner.java
+++ Unit 5/WordsCompareRunner.java
@@ -13,36 +13,36 @@ public class WordsCompareRunner
    public static void main( String args[] )
    {
 		WordsCompare word = new WordsCompare("abe", "ape");
-    word.compare();
-    System.out.println(word.toString());
+    		word.compare();
+    		System.out.println(word.toString());
 
-    WordsCompare word2 = new WordsCompare("giraffe", "gorilla");
-    word2.compare();
-    System.out.println(word2.toString());
+    		WordsCompare word2 = new WordsCompare("giraffe", "gorilla");
+    		word2.compare();
+    		System.out.println(word2.toString());
 
-    WordsCompare word3 = new WordsCompare("one", "two");
-    word3.compare();
-    System.out.println(word3.toString());
+    		WordsCompare word3 = new WordsCompare("one", "two");
+    		word3.compare();
+    		System.out.println(word3.toString());
 
-    WordsCompare word4 = new WordsCompare("fun", "funny");
-    word4.compare();
-    System.out.println(word4.toString());
+    		WordsCompare word4 = new WordsCompare("fun", "funny");
+    		word4.compare();
+    		System.out.println(word4.toString());
 
-    WordsCompare word5 = new WordsCompare("123", "19");
-    word5.compare();
-    System.out.println(word5.toString());
+    		WordsCompare word5 = new WordsCompare("123", "19");
+    		word5.compare();
+    		System.out.println(word5.toString());
 
-    WordsCompare word6 = new WordsCompare("193", "1910");
-    word6.compare();
-    System.out.println(word6.toString());
+    		WordsCompare word6 = new WordsCompare("193", "1910");
+    		word6.compare();
+    		System.out.println(word6.toString());
 
-    WordsCompare word7 = new WordsCompare("goofy", "godfather");
-    word7.compare();
-    System.out.println(word7.toString());
+    		WordsCompare word7 = new WordsCompare("goofy", "godfather");
+    		word7.compare();
+    		System.out.println(word7.toString());
 
-    WordsCompare word8 = new WordsCompare("funnel", "fun");
-    word8.compare();
-    System.out.println(word8.toString());
+    		WordsCompare word8 = new WordsCompare("funnel", "fun");
+    		word8.compare();
+    		System.out.println(word8.toString());
 
 	 }
 }
