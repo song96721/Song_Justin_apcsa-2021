3df53c8c94668d51a9920bb36c4c49ac86fbb809
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-09 19:00:43 -0800

    Create StringOddOrEven.java

---
+++ Unit 5/StringOddOrEven.java
@@ -0,0 +1,50 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Justin Song
+//Date - 2/9/21
+//Class - APCSA Period 3
+//Lab  - StringOddOrEven
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class StringOddOrEven
+{
+	private String word;
+
+	public StringOddOrEven()
+	{
+    word = "cat";
+	}
+
+	public StringOddOrEven(String s)
+	{
+    word = s;
+	}
+
+	public void setString(String s)
+	{
+    word = s;
+	}
+
+ 	public boolean isEven()
+ 	{
+		return (word.length() % 2 == 0);
+	}
+
+ 	public String toString()
+ 	{
+    if (isEven())
+    {
+      String output = word + " is even." + "\n";
+      return output;
+    }
+    else
+    {
+      String output = word + " is odd." + "\n";
+      return output;
+    }
+    
+	}
+}
