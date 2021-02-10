9e8c0046060b6eb3da74b6f4f65e32935331ad34
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-09 17:37:31 -0800

    Update FirstLastVowel.java

--- Unit 5/FirstLastVowel.java
+++ Unit 5/FirstLastVowel.java
@@ -7,8 +7,8 @@
 public class FirstLastVowel
 {
    public static String go( String a )
-	{
-		if (a.substring(0,1).equals("a") || a.substring(0,1).equals("A") ||    a.substring(0,1).equals("e") || a.substring(0,1).equals("E") ||    a.substring(0,1).equals("i") || a.substring(0,1).equals("I") ||
+  {
+    if (a.substring(0,1).equals("a") || a.substring(0,1).equals("A") ||    a.substring(0,1).equals("e") || a.substring(0,1).equals("E") ||    a.substring(0,1).equals("i") || a.substring(0,1).equals("I") ||
         a.substring(0,1).equals("o") || a.substring(0,1).equals("O") ||a.substring(0,1).equals("u") || a.substring(0,1).equals("U") ||a.substring(a.length()-1).equals("a") || a.substring(a.length()-1).equals("A") || a.substring(a.length()-1).equals("e") || a.substring(a.length()-1).equals("E") || a.substring(a.length()-1).equals("i") || a.substring(a.length()-1).equals("I") ||
         a.substring(a.length()-1).equals("o") || a.substring(a.length()-1).equals("O") ||a.substring(a.length()-1).equals("u") || a.substring(a.length()-1).equals("U")) 
     {
@@ -18,5 +18,5 @@ public class FirstLastVowel
     {
       return "no";
     }
-	}
+  }
 }
