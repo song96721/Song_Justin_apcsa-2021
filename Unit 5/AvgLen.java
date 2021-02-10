4c9856d6c45992c6d01f7f3de23a045680a5f0fc
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-09 17:05:34 -0800

    Create AvgLen.java

---
+++ Unit 5/AvgLen.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+//Date - 2/9/21
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+    double A;
+    double B;
+    double C;
+		A = (double)a.length();
+    B = (double)b.length();
+    C = (A + B)/2;
+    return C;
+	}
+}
