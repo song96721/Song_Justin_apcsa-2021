80ff03577a8d2dc738322394d9cd1e5200baccca
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-09 17:36:35 -0800

    Create FirstLastVowel.java

---
+++ Unit 5/FirstLastVowel.java
@@ -0,0 +1,22 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+//Date - 2/9/21
+
+
+public class FirstLastVowel
+{
+   public static String go( String a )
+	{
+		if (a.substring(0,1).equals("a") || a.substring(0,1).equals("A") ||    a.substring(0,1).equals("e") || a.substring(0,1).equals("E") ||    a.substring(0,1).equals("i") || a.substring(0,1).equals("I") ||
+        a.substring(0,1).equals("o") || a.substring(0,1).equals("O") ||a.substring(0,1).equals("u") || a.substring(0,1).equals("U") ||a.substring(a.length()-1).equals("a") || a.substring(a.length()-1).equals("A") || a.substring(a.length()-1).equals("e") || a.substring(a.length()-1).equals("E") || a.substring(a.length()-1).equals("i") || a.substring(a.length()-1).equals("I") ||
+        a.substring(a.length()-1).equals("o") || a.substring(a.length()-1).equals("O") ||a.substring(a.length()-1).equals("u") || a.substring(a.length()-1).equals("U")) 
+    {
+      return "yes";
+    }
+    else
+    {
+      return "no";
+    }
+	}
+}
