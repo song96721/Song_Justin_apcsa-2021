0f64adf44671e0046862eedeeb5234ba082c1fcc
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-15 21:42:08 -0800

    Update CountPairs.java

--- Unit 6/CountPairs.java
+++ Unit 6/CountPairs.java
@@ -9,13 +9,13 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
-    for (int i = 1; i < str.length(); i++)
-    {
-      if (str.charAt(i) == str.charAt(i-1))
-      {
-        count++;
-      }
-    }
+    		for (int i = 1; i < str.length(); i++)
+    		{
+      			if (str.charAt(i) == str.charAt(i-1))
+      			{
+        			count++;
+      			}
+    		}
 		return count;
 	}
 }
