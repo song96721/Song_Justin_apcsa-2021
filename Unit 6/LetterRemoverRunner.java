2efa4b8f46eccdaceae9b8d0d96892bb5fa784cc
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-15 22:03:00 -0800

    Create LetterRemoverRunner.java

---
+++ Unit 6/LetterRemoverRunner.java
@@ -0,0 +1,23 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+
+import static java.lang.System.*;
+
+public class LetterRemoverRunner
+{
+	public static void main( String args[] )
+	{
+		LetterRemover sentence = new LetterRemover();
+    System.out.println(sentence);	
+    sentence = new LetterRemover("ssssssssxssssesssssesss", 's');
+    System.out.println(sentence);	
+    sentence = new LetterRemover("qwertyqwertyqwerty", 'a');
+    System.out.println(sentence);	
+    sentence = new LetterRemover("abababababa", 'b');
+    System.out.println(sentence);	
+    sentence = new LetterRemover("abaababababa", 'x');
+    System.out.println(sentence);	
+											
+	}
+}
