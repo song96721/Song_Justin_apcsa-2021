99ad5ab6e38c14aa3ff2856e3ad4c613846ff9d4
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-15 21:59:30 -0800

    Create LetterRemover.java

---
+++ Unit 6/LetterRemover.java
@@ -0,0 +1,49 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+   private String sentence;
+   private char lookFor;
+
+	public LetterRemover()
+	{
+    sentence = "I am Sam I am   a";
+		lookFor = 'a';
+	}
+
+  public LetterRemover(String s, char rem)
+  {
+    sentence = s;
+    lookFor = rem;
+  }
+
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned = "";
+    int i = 0;
+    while (i < sentence.length())
+    {
+      if (sentence.charAt(i) != lookFor)
+      {
+        cleaned += sentence.charAt(i);
+      }
+      i++;
+    }
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters() + "\n";
+	}
+}
