adbc92510df43bf802938fdabd3cae2ec8ec783c
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 17:19:36 -0800

    Update WordRunner.java

--- Unit 6/WordRunner.java
+++ Unit 6/WordRunner.java
@@ -9,18 +9,18 @@ public class WordRunner
 	public static void main ( String[] args )
 	{
 		Word w = new Word("Hello");
-    System.out.println(w);
+    		System.out.println(w);
 
-    w = new Word("World");
-    System.out.println(w);
+    		w = new Word("World");
+    		System.out.println(w);
 
-    w = new Word("JukeBox");
-    System.out.println(w);
+    		w = new Word("JukeBox");
+    		System.out.println(w);
 
-    w = new Word("TCEA");
-    System.out.println(w);
+    		w = new Word("TCEA");
+    		System.out.println(w);
 
-    w = new Word("UIL");
-    System.out.println(w);
+    		w = new Word("UIL");
+    		System.out.println(w);
 	}
 }
