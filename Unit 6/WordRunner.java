d45fa250d54e640c08f59cbe92c9083260dab9b9
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 17:19:19 -0800

    Create WordRunner.java

---
+++ Unit 6/WordRunner.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name Justin Song
+
+import static java.lang.System.*;
+
+public class WordRunner
+{
+	public static void main ( String[] args )
+	{
+		Word w = new Word("Hello");
+    System.out.println(w);
+
+    w = new Word("World");
+    System.out.println(w);
+
+    w = new Word("JukeBox");
+    System.out.println(w);
+
+    w = new Word("TCEA");
+    System.out.println(w);
+
+    w = new Word("UIL");
+    System.out.println(w);
+	}
+}
