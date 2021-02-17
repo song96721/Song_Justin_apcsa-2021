3a5af8cc99e9e0955cbb74796a6794226631fc25
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 17:00:00 -0800

    Create TriangleOneRunner.java

---
+++ Unit 6/TriangleOneRunner.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name Justin Song
+
+import static java.lang.System.*;
+
+public class TriangleOneRunner
+{
+	public static void main ( String[] args )
+	{
+		TriangleOne Triangle = new TriangleOne("hippo");
+    Triangle.print();
+
+    Triangle = new TriangleOne("abcd");
+    Triangle.print();
+
+    Triangle = new TriangleOne("it");
+    Triangle.print();
+
+    Triangle = new TriangleOne("a");
+    Triangle.print();
+
+    Triangle = new TriangleOne("chicken");
+    Triangle.print();
+	}
+}
