33284c7ef4a71c260a6dd2e8dd13becd4f7080d6
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 16:48:10 -0800

    Create PerfectRunner.java

---
+++ Unit 6/PerfectRunner.java
@@ -0,0 +1,41 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+
+import static java.lang.System.*; 
+
+public class PerfectRunner
+{
+	public static void main( String args[] )
+	{
+		Perfect num = new Perfect();
+    System.out.println(num.toString());
+
+    num =  new Perfect(45);
+    System.out.println(num.toString());
+
+    num =  new Perfect(6);
+    System.out.println(num.toString());
+
+    num =  new Perfect(14);
+    System.out.println(num.toString());
+
+    num =  new Perfect(8128);
+    System.out.println(num.toString());
+
+    num =  new Perfect(1245);
+    System.out.println(num.toString());
+
+    num =  new Perfect(33);
+    System.out.println(num.toString());
+
+    num =  new Perfect(28);
+    System.out.println(num.toString());
+
+    num =  new Perfect(27);
+    System.out.println(num.toString());
+
+    num =  new Perfect(33550336);
+    System.out.println(num.toString());										
+	}
+}
