d58ffbb4c04def3b30ae35730ce92ae28ea25822
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 16:45:18 -0800

    Create Perfect.java

---
+++ Unit 6/Perfect.java
@@ -0,0 +1,59 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Justin Song
+
+import static java.lang.System.*;
+
+public class Perfect
+{
+  private int number;
+
+	public Perfect()
+  {
+    number = 496;
+  }
+
+  public Perfect(int num)
+  {
+    number = num;
+  }
+	
+  public void setPerfect(int num)
+  {
+    number = num;
+  }
+
+	public boolean isPerfect()
+	{
+    int count = 0;
+    int i = 1;
+    while (i <= number/2)
+    {
+      if (number % i == 0)
+      {
+        count += i;
+      }
+      i++;
+    }
+    if (count == number)
+    {
+      return true;
+    } 
+    else
+    {
+      return false;
+    }
+	}
+
+  public String toString()
+  {
+    if(isPerfect())
+    {
+      return number + " is perfect";
+    }
+    else
+    {
+      return number + " is not perfect";
+    }
+  }	
+}
