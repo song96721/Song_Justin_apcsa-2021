b4719a9d2994160e7657c2a473162bf56eb02151
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-16 17:00:25 -0800

    Update TriangleOneRunner.java

--- Unit 6/TriangleOneRunner.java
+++ Unit 6/TriangleOneRunner.java
@@ -9,18 +9,18 @@ public class TriangleOneRunner
 	public static void main ( String[] args )
 	{
 		TriangleOne Triangle = new TriangleOne("hippo");
-    Triangle.print();
+    		Triangle.print();
 
-    Triangle = new TriangleOne("abcd");
-    Triangle.print();
+    		Triangle = new TriangleOne("abcd");
+    		Triangle.print();
 
-    Triangle = new TriangleOne("it");
-    Triangle.print();
+    		Triangle = new TriangleOne("it");
+    		Triangle.print();
 
-    Triangle = new TriangleOne("a");
-    Triangle.print();
+    		Triangle = new TriangleOne("a");
+    		Triangle.print();
 
-    Triangle = new TriangleOne("chicken");
-    Triangle.print();
+    		Triangle = new TriangleOne("chicken");
+    		Triangle.print();
 	}
 }
