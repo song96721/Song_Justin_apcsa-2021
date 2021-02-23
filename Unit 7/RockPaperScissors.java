72b6c7b6cc5da5e7a1cc24c0864674b794560a00
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-22 19:55:22 -0800

    Update RockPaperScissors.java

--- Unit 7/RockPaperScissors.java
+++ Unit 7/RockPaperScissors.java
@@ -12,72 +12,72 @@ public class RockPaperScissors
 
 	public RockPaperScissors()
 	{
-    playChoice = "";
-    compChoice = "";
+    		playChoice = "";
+    		compChoice = "";
 	}
 
 	public RockPaperScissors(String player)
 	{
-    playChoice = player;
+    		playChoice = player;
 	}
 
 	public void setPlayers(String player)
 	{
-    playChoice = player;
+    		playChoice = player;
 	}
 
 	public String determineWinner()
 	{
 		String winner="";
-    int number = (int)Math.random()*3;
+    		int number = (int)Math.random()*3;
 
-    if (number == 0)
-    {
-      compChoice = "R";
-    }
-    if (number == 1)
-    {
-      compChoice = "P";
-    }
-    else
-    {
-      compChoice = "S";
-    }
+    		if (number == 0)
+    		{
+      			compChoice = "R";
+    		}
+    		if (number == 1)
+    		{
+      			compChoice = "P";
+    		}
+    		else
+    		{
+      			compChoice = "S";
+    		}
 
-    if (playChoice.equals(compChoice))
-    {
-      winner = "!Draw game!";
-    }
-    else if (playChoice.equals("R") && number == 1)
-    {
-      winner = "!Computer wins <<Paper Covers Rock>>!";
-    }
-    else if (playChoice.equals("P") && number == 2)
-    {
-      winner = "!Computer wins <<Scissors Cuts Paper>>!";
-    }
-    else if (playChoice.equals("S") && number == 0)
-    {
-      winner = "!Computer wins <<Rock Breaks Scissors>>!";
-    }
-    else if (playChoice.equals("R") && number == 2)
-    {
-      winner = "!Player wins <<Rock Breaks Scissors>>!";
-    }
-    else if (playChoice.equals("P") && number == 0)
-    {
-      winner = "!Player wins <<Paper Covers Rock>>!";
-    }
-    else
-    {
-      winner = "!Player wins <<Scissors Cuts Paper>>!";
-    }
+    		if (playChoice.equals(compChoice))
+    		{
+      			winner = "!Draw game!";
+    		}
+    		else if (playChoice.equals("R") && number == 1)
+    		{
+      			winner = "!Computer wins <<Paper Covers Rock>>!";
+    		}
+    		else if (playChoice.equals("P") && number == 2)
+    		{
+      			winner = "!Computer wins <<Scissors Cuts Paper>>!";
+    		}
+    		else if (playChoice.equals("S") && number == 0)
+    		{
+      			winner = "!Computer wins <<Rock Breaks Scissors>>!";
+    		}
+    		else if (playChoice.equals("R") && number == 2)
+    		{
+      			winner = "!Player wins <<Rock Breaks Scissors>>!";
+    		}
+    		else if (playChoice.equals("P") && number == 0)
+    		{
+      			winner = "!Player wins <<Paper Covers Rock>>!";
+    		}
+    		else
+    		{
+      			winner = "!Player wins <<Scissors Cuts Paper>>!";
+    		}
 		return winner;
 	}
 
 	public String toString()
 	{
-    String winner = determineWinner();
+    		String winner = determineWinner();
 		String output="player had " + playChoice + "\ncomputer had " + compChoice + "\n" + winner;
 		return output;
 	}
