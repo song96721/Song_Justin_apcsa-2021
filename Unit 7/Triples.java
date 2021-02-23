9554e3b5ca471cc797ab9f4f810558ca62a5b22f
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-22 20:05:25 -0800

    Update Triples.java

--- Unit 7/Triples.java
+++ Unit 7/Triples.java
@@ -15,45 +15,45 @@ public class Triples
 
 	public Triples(int num)
 	{
-    number = num;
+    		number = num;
 	}
 
 	public void setNum(int num)
 	{
-    number = num;
+    		number = num;
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
 		int max = 1;
-    int i = 1;
-    while (i <= (Math.max(c, Math.max(a, b))/2))
-    {
-      if (a % i == 0 && b % i == 0 && c % i == 0)
-      {
-        max = i;
-      }
-      i++;
-    }
-    return max;
+    		int i = 1;
+    		while (i <= (Math.max(c, Math.max(a, b))/2))
+    		{
+      			if (a % i == 0 && b % i == 0 && c % i == 0)
+      			{
+        			max = i;
+      			}
+      			i++;
+    		}
+    		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
-    for (int a = 1; a <= number; a++)
-    {
-      for (int b = 1; b <= number; b++)
-      {
-        for (int c = 1; c <= number; c++)
-        {
-          if (Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2) && ((a % 2 == 0 && b % 2 == 1)||(a % 2 == 1 && b % 2 == 0)) && c % 2 == 1 && greatestCommonFactor(a, b, c) == 1)
-          {
-            output += String.format("%1$s %2$s %3$s\n",a,b,c);
-          }
-        }
-      }
+    		for (int a = 1; a <= number; a++)
+    		{
+      			for (int b = 1; b <= number; b++)
+      			{
+        		for (int c = 1; c <= number; c++)
+        		{
+          			if (Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2) && ((a % 2 == 0 && b % 2 == 1)||(a % 2 == 1 && b % 2 == 0)) && c % 2 == 1 && greatestCommonFactor(a, b, c) == 1)
+          			{
+            				output += String.format("%1$s %2$s %3$s\n",a,b,c);
+          			}
+        		}
+      		}
+    	}
+	return output+"\n";
     }
-	  return output+"\n";
-	}
 }
