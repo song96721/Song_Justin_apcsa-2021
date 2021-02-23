a8152751f4923151b01ffd1e03e388dbe1fc39e9
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-22 19:51:37 -0800

    Update DoubleRunner.java

--- Unit 7/DoubleRunner.java
+++ Unit 7/DoubleRunner.java
@@ -12,23 +12,23 @@ public class DoubleRunner
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
-    run = new BiggestDouble(14.51, 6.17, 71.8, 1.0);
+    		run = new BiggestDouble(14.51, 6.17, 71.8, 1.0);
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
-    run = new BiggestDouble(41.15, 816.7, 17.8, 19.0);
+    		run = new BiggestDouble(41.15, 816.7, 17.8, 19.0);
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
-    run = new BiggestDouble(884.5, 16.7, 7.8, 9.0);
+    		run = new BiggestDouble(884.5, 16.7, 7.8, 9.0);
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
-    run = new BiggestDouble(4.5, -456.7, 677.8, 9.0);
+    		run = new BiggestDouble(4.5, -456.7, 677.8, 9.0);
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 
-    run = new BiggestDouble(4.5, 16.7, -7.8, -9.0);
+    		run = new BiggestDouble(4.5, 16.7, -7.8, -9.0);
 		System.out.println(run);
 		System.out.println("biggest = " + run.getBiggest() + "\n");
 				
