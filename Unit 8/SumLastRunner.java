cde044d68c82e24012fbfd3b3e31d68907ff9ef0
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-24 23:10:38 -0800

    Update SumLastRunner.java

--- Unit 8/SumLastRunner.java
+++ Unit 8/SumLastRunner.java
@@ -7,7 +7,7 @@ public class SumLastRunner
 {
 	public static void main( String args[] )
 	{		
-    int array[] = {-99,1,2,3,4,5,6,7,8,9,10,5};
+    		int array[] = {-99,1,2,3,4,5,6,7,8,9,10,5};
 		System.out.println(RaySumLast.go(array));
 		
 		int array1[] = {10,9,8,7,6,5,4,3,2,1,-99};
