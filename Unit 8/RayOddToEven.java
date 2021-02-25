5177123113e6ebc770db78ace2488f4dd2455053
song96721 <78109443+song96721@users.noreply.github.com>
2021-02-24 23:09:59 -0800

    Update RayOddToEven.java

--- Unit 8/RayOddToEven.java
+++ Unit 8/RayOddToEven.java
@@ -11,32 +11,32 @@ public class RayOddToEven
 	}
 	
 	public static int checker(int arr[]) 
-  {
+  	{
 		int odd = 0;
 		int even = 0;
 
 		for (int i = 0;i < arr.length;i++) 
-    {
+    		{
 			if (arr[i] % 2 == 1) 
-      {
+      			{
 				odd = i;
 				break;
 			}
 		}
 		for (int i = odd + 1;i < arr.length;i++) 
-    {
+    		{
 			if (arr[i] % 2 == 0) 
-      {
+      			{
 				even = i;
 				break;
 			}
 		}
 		if(even - odd > 0) 
-    {
+    		{
 			return even - odd;
 		}
 		else 
-    {
+    		{
 			return -1;
 		}
 	}
