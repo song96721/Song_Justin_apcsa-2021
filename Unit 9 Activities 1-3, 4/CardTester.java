05db1bd1da28b40d60fbe3734a4c370c99682575
song96721 <78109443+song96721@users.noreply.github.com>
2021-03-08 19:59:34 -0800

    Update CardTester.java

--- Unit 9 Activities 1-3, 4/CardTester.java
+++ Unit 9 Activities 1-3, 4/CardTester.java
@@ -3,13 +3,13 @@
 public class CardTester 
 {
 	public static void main(String[] args) 
-  {
-    //Create three Card objects and test each method for each Card object.
-    Card one = new Card("9", "Clover", 9);
-    Card two = new Card("2", "Hearts",2);
-    Card three = new Card("5", "Spades", 5);
+  	{
+    		//Create three Card objects and test each method for each Card object.
+    		Card one = new Card("9", "Clover", 9);
+    		Card two = new Card("2", "Hearts",2);
+    		Card three = new Card("5", "Spades", 5);
   
-    System.out.println(one.suit());
+    		System.out.println(one.suit());
 		System.out.println(one.rank());
 		System.out.println(one.pointValue());
 		System.out.println(one.matches(two));
