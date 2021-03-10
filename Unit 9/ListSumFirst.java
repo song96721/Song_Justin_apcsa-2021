f33a442352cf0cab9719a5e71132e6c56c0048f7
song96721 <78109443+song96721@users.noreply.github.com>
2021-03-10 15:47:58 -0800

    Update ListSumFirst.java

--- Unit 9/ListSumFirst.java
+++ Unit 9/ListSumFirst.java
@@ -11,26 +11,26 @@ public class ListSumFirst
 	public static int go(List<Integer> ray)
 	{
 		if (ray.size() == 0) 
-    {
+    		{
 			return -1;
 		}
 		int count = 0;
 		int i = 0;
 		for(int x:ray) 
-    {
+    		{
 			if (x > ray.get(0)) 
-      {
+      			{
 				count = count + x;
 				i++;
 			}
 		}
 		if (i == 0) 
-    {
+    		{
 			return -1;
 		}
 		else
-    {
-      return count;
-    }
+    		{
+      			return count;
+    		}
 	}
 }
