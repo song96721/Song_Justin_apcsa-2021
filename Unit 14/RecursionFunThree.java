0ec907f8872967f67a8caf0452572706ce62c2b2
song96721 <78109443+song96721@users.noreply.github.com>
2021-04-14 23:15:59 -0800

    Update RecursionFunThree.java

--- Unit 14/RecursionFunThree.java
+++ Unit 14/RecursionFunThree.java
@@ -20,7 +20,7 @@ public class RecursionFunThree
 	 */
 	public static int luckyThrees( long number )
 	{
-    if (number > 0)
+    		if (number > 0)
 			if (number / 10 == 0) return luckyThrees(number/10);
 			else if (number % 10 == 3) return 1 + luckyThrees(number/10);
 			else return luckyThrees(number/10);
